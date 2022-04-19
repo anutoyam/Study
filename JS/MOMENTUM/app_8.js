c32e1439bb8adc8fc28a217151ea0a49870f289b
anutoyam <akflshdev@gmail.com>
2022-04-19 16:10:00 +0900

    22.04.19_1

--- JS/MOMENTUM/app_8.js
+++ JS/MOMENTUM/app_8.js
@@ -1,4 +1,8 @@
+//
+const title = document.querySelector("div.hello:first-child h1");
 
-const title = document.querySelector('.hello h1');
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
+title.addEventListener("click",handleTitleClick);
 
-console.log(title);
