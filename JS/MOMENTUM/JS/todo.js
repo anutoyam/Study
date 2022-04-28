16837612449a41c6685332574f31f28eff92ba8f
anutoyam <akflshdev@gmail.com>
2022-04-28 15:59:38 +0900

    22.04.28_1

--- JS/MOMENTUM/JS/todo.js
+++ JS/MOMENTUM/JS/todo.js
@@ -2,8 +2,36 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event){
+function deleteTodo(event) {
+    
+    //내가 짠 코드 역순으로 지워도 순서대로 지워지는 오류가 있음.
+    //event.preventDefault();
+    //const li = toDoList.querySelector("li");
+    //console.dir(event.target.parentElement);
+    //toDoList.removeChild(li);
+
+    //강의 코드
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
     event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
