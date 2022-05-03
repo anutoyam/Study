0d752f97bc0f195634b9b3878a4e211a87b3d9b9
anutoyam <akflshdev@gmail.com>
2022-05-03 16:07:28 +0900

    22.05.03_1

--- JS/MOMENTUM/JS/todo.js
+++ JS/MOMENTUM/JS/todo.js
@@ -4,16 +4,18 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 function deleteTodo(event) {
-
-    // 내가 짠 코드 역순으로 지워도 순서대로 지워지는 오류가 있음. event.preventDefault(); const li =
-    // toDoList.querySelector("li"); console.dir(event.target.parentElement);
+    // 내가 짠 코드 역순으로 지워도 순서대로 지워지는 오류가 있음. 
+    // event.preventDefault(); 
+    // const li = toDoList.querySelector("li"); 
+    //console.dir(event.target.parentElement);
+    
     //toDoList.removeChild(li); 강의 코드
     const li = event.target.parentElement;
     li.remove();
@@ -21,8 +23,9 @@ function deleteTodo(event) {
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
@@ -35,21 +38,34 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    //중복값을 분류하기 위해 데이터마다 ID를 부여한다.
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-function sayHello(item) {
-    console.log("this is the turn of",item);
-}
+// solution 1) function으로 구현하기
+// function sayHello(item) {
+//     console.log("this is the turn of",item);
+// }
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
     console.log(parsedToDos);
     // parsedToDos.forEach(sayHello);
-    parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+    toDos = parsedToDos;
+    //solution 2) foreach로 구현하기
+    parsedToDos.forEach(paintTodo);
+
+    //내가 짠 코드
+    // for(i=0; i<parsedToDos.length; i++){
+    //     paintTodo(parsedToDos[i].text);
+    // }
 }
