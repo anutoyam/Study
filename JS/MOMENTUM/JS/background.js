4abb900d5a2755c2ba6aaa886f671ad6eaff768a
anutoyam <akflshdev@gmail.com>
2022-06-16 16:04:53 +0900

    22.06.16_1

--- JS/MOMENTUM/JS/background.js
+++ JS/MOMENTUM/JS/background.js
@@ -4,6 +4,8 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
+
 //prepend 제일 위에 추가 append 제일 아래에 추가
 document.body.appendChild(bgImage);
+
 //document.body.prepend(bgImage);
