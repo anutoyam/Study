6b10a923a6a0e07ad97adbf5a0faa711bc3e1783
anutoyam <akflshdev@gmail.com>
2022-07-07 17:24:16 +0900

    22.07.07_3

--- REACT/react-app/src/App.js
+++ REACT/react-app/src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return <header>
+  <h1><a href='/'>React</a></h1>
+</header>
+}
+
+function Nav() {
+  return <nav>
+  <li><a href="/read/1">html</a></li>
+  <li><a href="/read/2">css</a></li>
+  <li><a href="/read/3">js</a></li>
+</nav>
+}
+
+function Article() {
+  return<article>
+  <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <h1><a href='/'>WEB</a></h1>
-      </header>
-    <nav>
-      
-    </nav>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
