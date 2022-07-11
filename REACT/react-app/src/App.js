d8b746130105175e9c89222edf77e3d23111116b
anutoyam <akflshdev@gmail.com>
2022-07-11 09:55:11 +0900

    22.07.11_1

--- REACT/react-app/src/App.js
+++ REACT/react-app/src/App.js
@@ -1,35 +1,36 @@
 import './App.css';
 
-function Header() {
+function Header(props) {
   return <header>
-  <h1><a href='/'>React</a></h1>
+  <h1><a href='/'>{props.title}</a></h1>
 </header>
 }
 
-function Nav() {
+function Nav(props ) {
   return <nav>
-  <li><a href="/read/1">html</a></li>
-  <li><a href="/read/2">css</a></li>
-  <li><a href="/read/3">js</a></li>
+  <li><a href="/read/{props.id}">{props.title}</a></li>
 </nav>
 }
 
-function Article() {
+function Article(props) {
   return<article>
-  <h2>Welcome</h2>
-    Hello, WEB
+  <h2>{props.title}</h2>
+    {props.body}
   </article>
 
 }
 
 function App() {
+  const topic = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
   return (
     <div>
-      <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title = "React test!"></Header>
+      <Nav ></Nav>
+      <Article title = "Welcome" body = "Hello, Web!"></Article>
     </div>
   );
 }
