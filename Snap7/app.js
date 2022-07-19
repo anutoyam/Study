467e0307b488ecd71f2cecbaf9f14d4fc8130cbf
anutoyam <akflshdev@gmail.com>
2022-07-19 16:01:27 +0900

    22.07.19_2

--- Snap7/app.js
+++ Snap7/app.js
@@ -20,6 +20,10 @@ const server = app.listen(3000, function () {
     plcRead();
 })
 
+window.onload = function(){
+
+}
+
 function plcRead() {
     var s7client = new snap7.S7Client();
     s7client.ConnectTo('192.168.0.95', 0, 2, function (err) {
@@ -42,4 +46,37 @@ function plcRead() {
         });
     });
     s7client.Disconnect();
+}
+
+// worker 실행
+function startWorker() {
+
+    // Worker 지원 유무 확인
+    if (!!window.Worker) {
+
+        // 실행하고 있는 워커 있으면 중지시키기
+        if (worker) {
+            stopWorker();
+        }
+
+        worker = new Worker('worker.js');
+        worker.postMessage('워커 실행'); // 워커에 메시지를 보낸다.
+
+        // 메시지는 JSON구조로 직렬화 할 수 있는 값이면 사용할 수 있다. Object등 worker.postMessage( { name :
+        // '302chanwoo' } ); 워커로 부터 메시지를 수신한다.
+        worker.onmessage = function (e) {
+            console.log('호출 페이지 - ', e.data);
+            output.innerHTML += e.data;
+        };
+    }
+
+}
+// worker 중지
+function stopWorker() {
+
+    if (worker) {
+        worker.terminate();
+        worker = null;
+    }
+
 }
