79a47cc03c574c0f13183fab41ac115903472d26
anutoyam <akflshdev@gmail.com>
2022-07-21 16:06:33 +0900

    22.07.21_1

--- Snap7/app.js
+++ Snap7/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
-const ejs = require('ejs');
 const snap7 = require('node-snap7');
-var arr = new Array(4);
+
+const app = express();
+//word byte
+const arrWord = new Array(24);
+const s7client = new snap7.S7Client();
 
 app.set('views', __dirname + '/view')
 app.set('view engine', 'ejs')
@@ -10,73 +12,61 @@ app.engine('html', require('ejs').renderFile)
 
 app.get('/', (req, res) => {
     res.render('index', {
-        name: '홍길동',
-        array: arr
+        name: '이문환',
+        array: arrWord
     })
 })
 
-const server = app.listen(3000, function () {
+app.listen(3000, function () {
     console.log("port 3000");
+    plcConnect();
     plcRead();
+    plcDisconnect();
 })
 
-window.onload = function(){
-
-}
-
-function plcRead() {
-    var s7client = new snap7.S7Client();
+function plcConnect() {
     s7client.ConnectTo('192.168.0.95', 0, 2, function (err) {
-
         if (err) 
             return console.log(
                 '>> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err)
             );
-        
-        s7client.DBRead(59, 2, 8, function (err, res) {
-            if (err) 
-                return console.log(
-                    '>> DBRead Failed. Code #' + err + ' - ' + s7client.ErrorText(err)
-                );
-            
-            for (let i = 0; i < res.length / 2; i++) {
-                length = res.length / 2
-                arr[i] = res[i * 2 + 1];
-            }
-        });
-    });
+        }
+    )
+};
+
+function plcDisconnect() {
     s7client.Disconnect();
 }
 
-// worker 실행
-function startWorker() {
+function plcRead() {
 
-    // Worker 지원 유무 확인
-    if (!!window.Worker) {
-
-        // 실행하고 있는 워커 있으면 중지시키기
-        if (worker) {
-            stopWorker();
+    s7client.DBRead(59, 0, 48, function (err, res) {
+        if (err) 
+            return console.log(
+                '>> DBRead Failed. Code #' + err + ' - ' + s7client.ErrorText(err)
+            );
+        
+        for (let i = 0; i < res.length / 2; i++) {
+            length = res.length / 2
+            arrWord[i] = res[i * 2 + 1];
         }
+        console.log(res);
+    });
+    
 
-        worker = new Worker('worker.js');
-        worker.postMessage('워커 실행'); // 워커에 메시지를 보낸다.
+    s7client.DBRead(59, 48, 1, function (err, res) {
+        if (err) 
+            return console.log(
+                '>> DBRead Failed. Code #' + err + ' - ' + s7client.ErrorText(err)
+            );
 
-        // 메시지는 JSON구조로 직렬화 할 수 있는 값이면 사용할 수 있다. Object등 worker.postMessage( { name :
-        // '302chanwoo' } ); 워커로 부터 메시지를 수신한다.
-        worker.onmessage = function (e) {
-            console.log('호출 페이지 - ', e.data);
-            output.innerHTML += e.data;
-        };
-    }
+            console.log(res[0]);
 
-}
-// worker 중지
-function stopWorker() {
+    });
 
-    if (worker) {
-        worker.terminate();
-        worker = null;
-    }
-
+    // setTimeout( function() {
+    //     plcRead();
+    //     console.log("-----------------");
+    // }, 500 );
+    
 }
