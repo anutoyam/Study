ffbd6629752525b2af9dadc8eecaca3d513abc84
anutoyam <akflshdev@gmail.com>
2022-09-28 10:38:57 +0900

    22.09.28_1

--- Python/py_Main_serial_graph_test.py
+++ Python/py_Main_serial_graph_test.py
@@ -24,13 +24,19 @@ bit = []
 data = []
 cycle = 0
 status = True
-profile = 1
+profile = 4
 
 #window 선언
 root = tk.Tk()
 root.title('SPICA Sensor Comm')
 root.resizable(width=False, height=False)
 
+vari1 = tk.IntVar()
+vari2 = tk.IntVar()
+vari3 = tk.IntVar()
+vari4 = tk.IntVar()
+vari5 = tk.IntVar()
+vari6 = tk.IntVar()
 
 fig = plt.figure(figsize=(12,8))
 plt.subplots_adjust(left=0.35)
@@ -38,11 +44,9 @@ ax = plt.axes(xlim=(0, 127), ylim=(0, 255))
 line, = ax.plot([], [], lw=1)
 anim = None
 
-var = tk.StringVar()
-
 #region FUNC
 def animate(event):
-    global profile,var,frame2
+    global profile,vari1,vari2,vari3,vari4,vari5,vari6
     if profile != 4 :
         data = py_Serial_1.serial_recieve(ser,profile)
         Ydata = list(data)
@@ -52,14 +56,14 @@ def animate(event):
         return line,
     elif profile == 4 :
         data = py_Serial_1.serial_recieve(ser,profile) 
-
         # 슬립타임마다 렉이걸려서 쓰레드 고려
-        var.set('123123')
-        print(data[0])
-        frame2.update_idletasks()
-        time.sleep(0.5)
+        vari1.set(data[0])
+        vari2.set(data[1])
+        vari3.set(data[2])
+        vari4.set(data[3])
+        vari5.set(data[4])
+        vari6.set(data[5])
         
-
 def btnfunc(label):
     global status, ax, anim, profile
     if status == True :
@@ -98,7 +102,6 @@ frame2 = tk.Frame(root)
 frame1.grid(column=0, row=1, sticky="nsew")
 frame2.grid(column=0, row=1, sticky="nsew")
 
-
 # ''' Add Button '''
 # Period BTN
 resetax1 = plt.axes([0.05, 0.78, 0.2, 0.04])
@@ -121,22 +124,49 @@ mybox = {'facecolor':'y','edgecolor':'r','boxstyle':'round','alpha':0.5}
 plt.text(0.05,15,('Period Scan'),fontsize = 11, bbox=mybox)
 plt.text(0.05,4,('OneShot Scan'),fontsize = 11, bbox=mybox)
 
+# '''Measure Scan'''
+label1 = tk.Label(frame2, width= 50, height= 5)
+label1.pack(fill='both')
+label1_1 = tk.Label(label1, text = 'Status 1 :',font=(30))
+label1_1.pack(side='left')
+label1_2 = tk.Label(label1, textvariable=vari1 ,font=(30))
+label1_2.pack(side='left')
 
+label2 = tk.Label(frame2, width= 50, height= 5)
+label2.pack(fill='both')
+label2_1 = tk.Label(label2, text = 'Status :',font=(30))
+label2_1.pack(side='left')
+label2_2 = tk.Label(label2, textvariable=vari2 ,font=(30))
+label2_2.pack(side='left')
 
-label1 = tk.Label(frame2, text = 'Status 1 :',font=(15))
-label1.pack(anchor='w')
-label7 = tk.Label(frame2, textvariable=var ,font=(15))
+label3 = tk.Label(frame2, width= 50, height= 5)
+label3.pack(fill='both')
+label3_1 = tk.Label(label3, text = 'Position :',font=(30))
+label3_1.pack(side='left')
+label3_2 = tk.Label(label3, textvariable=vari3 ,font=(30))
+label3_2.pack(side='left')
 
-label2 = tk.Label(frame2, text = 'Status :',font=(15))
-label2.pack(anchor='w')
-label3 = tk.Label(frame2, text = 'Position :',font=(15))
-label3.pack(anchor='w')
-label4 = tk.Label(frame2, text = 'Energy :',font=(15))
-label4.pack(anchor='w')
-label5 = tk.Label(frame2, text = 'UNDEF :',font=(15))
-label5.pack(anchor='w')
-label6 = tk.Label(frame2, text = 'UNDEF :',font=(15))
-label6.pack(anchor='w')
+label4 = tk.Label(frame2, width= 50, height= 5)
+label4.pack(fill='both')
+label4_1 = tk.Label(label4, text = 'Energy :',font=(30))
+label4_1.pack(side='left')
+label4_2 = tk.Label(label4, textvariable=vari4 ,font=(30))
+label4_2.pack(side='left')
+
+label5 = tk.Label(frame2, width= 50, height= 5)
+label5.pack(fill='both')
+label5_1 = tk.Label(label5, text = 'UNDEF :',font=(30))
+label5_1.pack(side='left')
+label5_2 = tk.Label(label5, textvariable=vari5 ,font=(30))
+label5_2.pack(side='left')
+
+label6 = tk.Label(frame2, width= 50, height= 5)
+label6.pack(fill='both')
+label6_1 = tk.Label(label6, text = 'UNDEF :',font=(30))
+label6_1.pack(side='left')
+label6_2 = tk.Label(label6, textvariable=vari6 ,font=(30))
+label6_2.pack(side='left')
+
 
 # Frame BTN
 f = font.Font(size=15, weight='bold')
@@ -202,6 +232,7 @@ if __name__ == '__main__' :
 
     btnToFrame1.pack(fill='x')
     btnToFrame2.pack(fill='x',side='bottom', anchor='s',expand= True)
+    
     openFrame(frame1)
     tk.mainloop()
 
